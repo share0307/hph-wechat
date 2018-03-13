1e6bf205ad59a52b81bd0c28148bd49d63ed0ffd
kkk <kkk@kkk.com>
2018-03-13 16:45:31 +0800

    测试

--- www/wechat.services.bx.com/app/Http/Controllers/Api/WechatController.php
+++ www/wechat.services.bx.com/app/Http/Controllers/Api/WechatController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
 
 class WechatController extends Controller{
 
@@ -11,8 +12,13 @@ class WechatController extends Controller{
      * 验证服务器
      * @author  jianwei
      */
-    function test(Request $request){
-        return $request->get("echostr");
+    function test(Request $request,Response $response){
+        $echostr = "hello wordl!";
+        if($request->has('echostr')) {
+            $echostr = $request->get("echostr");
+        }
+        
+        return $echostr->setContent($echostr);
     }
 
 
